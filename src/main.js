5e322d00679aa9da86eda709cd93800d3031a99b
Opeyemi Dada <dadaadeife@gmail.com>
2024-07-25 12:28:31 +0100

    setup

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { createPinia } from "pinia";
 
-createApp(App).use(router).mount('#app')
+
+import "./styles/index.css";
+import "./styles/main.css";
+import "vue3-toastify/dist/index.css";
+
+const pinia = createPinia();
+
+createApp(App).use(pinia).use(router).mount("#app");
